32da431d00ab71448cc1e9f1ca6d82ddc737ef79
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-02-21 10:14:11 +0100

    step2: add admin

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import AdminBro from 'admin-bro';
+import AdminBroMongoose from 'admin-bro-mongoose';
+import { buildRouter } from 'admin-bro-expressjs';
+
+AdminBro.registerAdapter(AdminBroMongoose);
 
 const app = express();
 const port = 3000;
@@ -11,6 +16,11 @@ const run = async () => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
+  const adminBro = new AdminBro({});
+  const router = buildRouter(adminBro);
+  app.use(adminBro.options.rootPath, router);
+
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 };
 
